34a44bdcdd7f2df8f2592f0850865b8741789437
almeidarenato <renatoam@ymail.com>
2020-01-19 03:14:14 -0300

    adicionado dependencia cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 
 mongoose.connect(
   "mongodb://omnistack:omnistack@cluster0-shard-00-00-jyjzr.mongodb.net:27017,cluster0-shard-00-01-jyjzr.mongodb.net:27017,cluster0-shard-00-02-jyjzr.mongodb.net:27017/semana10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
@@ -35,7 +36,8 @@ app.put("/users/:id", (req, res) => {
 */
 /* use para toda a aplicaçao entender oque será usado */
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3338);
+app.listen(3339);
